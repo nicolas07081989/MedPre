c36ba88d248660f7d856de0fc19153393e3a9649
nicolas07081989 <info@academiabauer.com>
2024-12-27 14:30:43 -0500

    Add all SvelteKit configuration files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+﻿import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: ['static']
-		}
-	}
-}); 
+  plugins: [sveltekit()]
+});
