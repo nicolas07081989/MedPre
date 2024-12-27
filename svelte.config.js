733a2189da2372a84f74f5fab9dfd80b13e47c1e
nicolas07081989 <info@academiabauer.com>
2024-12-27 14:59:21 -0500

    Update SvelteKit and Vercel configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-﻿import adapter from "@sveltejs/adapter-vercel";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   kit: {
     adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+  }
 };
 
 export default config;
